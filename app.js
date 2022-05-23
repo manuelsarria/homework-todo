ea0be09a9f6a03c89fd4c2f02812245e6756f012
Manuel Sarrria <manuelsarria@Manuels-MacBook-Pro.local>
2022-05-22 19:33:24 -0500

    commit final

--- app.js
+++ app.js
@@ -1,67 +1,72 @@
+require("colors");
 
+const { guardarDB, leerDb } = require("./helpers/guardarArchivo");
+const {
+  inquirerMenu,
+  pausa,
+  leerInput,
+  listadoTareasBorrar,
+  confirmar,
+  mostrarListadoChecklist,
+} = require("./helpers/inquirer");
+const Tarea = require("./models/tarea");
+const Tareas = require("./models/tareas");
 
-require('colors');
+const main = async () => {
+  console.log("hola mundo");
 
-const { guardarDB, leerDb } = require('./helpers/guardarArchivo');
-const { inquirerMenu, pausa, leerInput, listadoTareasBorrar } = require('./helpers/inquirer');
-const Tarea = require('./models/tarea');
-const Tareas = require('./models/tareas');
+  let opt = "";
 
-const main = async() => {
+  const tareas = new Tareas();
 
-    console.log('hola mundo');
+  const tareaDB = leerDb();
 
-    let opt = '';
+  if (tareaDB) {
+    tareas.cargarTareasForArray(tareaDB);
+  }
 
-    const tareas = new Tareas();
+  do {
+    opt = await inquirerMenu();
 
-    const tareaDB = leerDb();
-
-    if (tareaDB) {
-        tareas.cargarTareasForArray(tareaDB);
+    switch (opt) {
+      case "1":
+        //crear opcion
+        const desc = await leerInput("Descripcion: ");
+        tareas.crearTarea(desc);
+        break;
+      case "2":
+        tareas.listadoTareas();
+        break;
+      case "3":
+        tareas.listarPendientesCompletadas(true);
+        break;
+      case "4":
+        tareas.listarPendientesCompletadas(false);
+        break;
+      case "5":
+        const ids = await mostrarListadoChecklist(tareas.listadoArr);
+        tareas.toggleCompletadas( ids );
+        break;
+      case "6":
+        const id = await listadoTareasBorrar(tareas.listadoArr);
+        const ok = await confirmar("estas seguro?");
+        if (ok) {
+          tareas.borrarTarea(id);
+          console.log("Tarea borrada");
+        }
+        break;
     }
 
-
-    do {
-
-        opt = await inquirerMenu();
-
-        switch (opt) {
-            case '1':
-                //crear opcion
-                const desc = await leerInput('Descripcion: ')
-                tareas.crearTarea(desc);
-                break;
-            case '2':
-                tareas.listadoTareas();
-                break;
-            case '3':
-                tareas.listarPendientesCompletadas(true);
-                break;
-            case '4':
-                tareas.listarPendientesCompletadas(false);
-                break;
-            case '5':
-                tareas.listarPendientesCompletadas(false);
-                break;
-            case '6':
-                const id = await listadoTareasBorrar(tareas.listadoArr);
-
-                break;
-    
-        }
-
-        guardarDB(tareas.listadoArr);
+    guardarDB(tareas.listadoArr);
 
     //    const tarea = new Tarea('comprar comida');
     //    console.log(tarea);
 
-       if (opt !== '0') await pausa();
+    if (opt !== "0") await pausa();
 
-       console.log({ opt });
-        
-    } while (opt !== '0');
+    console.log({ opt });
+  } while (opt !== "0");
 
-}
+};
 
 main();
